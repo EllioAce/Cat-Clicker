9a86d23697a0d7d5c350cbf8498f97c62b3a328d
EllioAce <elliott.elijah12@gmail.com>
2023-11-09 14:44:50 +0200

    Initial commit

---
+++ components/ClickComponent.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System.Drawing;
+
+[GlobalClass()]
+public partial class ClickComponent : Area2D
+{
+    [Signal]
+    public delegate void OnClickEventHandler();
+    public CollisionShape2D collider;
+    bool mouseHovering;
+    public bool MouseHovering { get => mouseHovering; }
+    public override void _Ready()
+    {
+        collider = GetChild<CollisionShape2D>(0);
+    }
+    public override void _Process(double delta)
+    {
+        mouseHovering = MouseInsideArea();
+    }
+    bool MouseInsideArea()
+    {
+        Vector2 mousePosition = GetLocalMousePosition();
+        Rect2 mouseRectangle = new Rect2(mousePosition, Vector2.One);
+        Rect2 shapeRect = collider.Shape.GetRect();
+
+        return shapeRect.Intersects(mouseRectangle);
+    }
+}
